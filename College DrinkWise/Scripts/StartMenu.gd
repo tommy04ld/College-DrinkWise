891366a7a3b57201ad37c23174e80cc2347374a8
Tommy Cunningham <tommy04ld@gmail.com>
2023-12-06 15:36:52 -0500

    idk

--- College DrinkWise/Scripts/StartMenu.gd
+++ College DrinkWise/Scripts/StartMenu.gd
@@ -9,7 +9,6 @@ func _on_NewGame_pressed():
 	$AnimationPlayer.play("fade_to_black")
 
 
-
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if anim_name == "fade_to_black":
 		get_tree().change_scene_to(load('res://Scenes/World.tscn'))
